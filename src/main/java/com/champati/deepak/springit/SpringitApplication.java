e078c5f4d7023bcbcc4e46105938082b62bbe4c5
Deepak Champatiray <dechamp6@in.ibm.com>
2019-10-06 13:11:48 +0530

    Profiles, Properties and ConfigurationProperties

--- src/main/java/com/champati/deepak/springit/SpringitApplication.java
+++ src/main/java/com/champati/deepak/springit/SpringitApplication.java
@@ -1,14 +1,33 @@
 package com.champati.deepak.springit;
 
+import com.champati.deepak.springit.config.SpringItProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringItProperties.class)
 public class SpringitApplication {
+
+	@Autowired
+	private SpringItProperties props;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 		System.out.println("Welcome to Spring Boot Application.");
+	}
+
+	@Bean
+	//@Profile("dev")
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("Welcome Message : " + props.getWelcomeMsg());
+		};
 	}
 
 }
